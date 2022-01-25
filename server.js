5959e1300cf1c34674a13f892ea642902c2e0d52
emma ingurat <e.ingurat@bsmart-technology.com>
2022-01-25 12:16:03 +0300

    express handler fixes

--- server.js
+++ server.js
@@ -1,88 +1,31 @@
-// https://code.tutsplus.com/tutorials/using-passport-with-sequelize-and-mysql--cms-27537
-
 var express = require('express');
-var app = express();
-var passport = require('passport');
-var session = require('express-session');
-var bodyParser = require('body-parser');
-// var env = require('dotenv').load();
-var exphbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-// var flash = require('connect-flash');
-var path = require('path');
+const app = express();
+var passport = require('passport')
+var session = require('express-session')
+var bodyParser = require('body-parser')
+// var env = require('dotenv').load()
+var exphbs = require('express-handlebars')
+var models = require("./app/models");
 
-//For BodyParser
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+app.get('/', function(req, res) {
+    res.send('Welcome to Passport with Sequelize');
+});
 
- //For Passport
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true, cookie: { maxAge: 60000 }})); //session secret key
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
+// Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
 
-//Express Validator
-// app.use(expressValidator({
-//     errorFormatter: function(param, msg, value){
-//         var namespace = param.split(',')
-//         , root = namespace.shift()
-//         , formParam = root;
-
-//         while(namespace.length){
-//             formParam += '[' + namespace.shift() + ']';
-//         }
-//         return{
-//             param: formParam,
-//             msg: msg,
-//             value :value
-//         };
-//     }
-// }));
-
-//connect flash
-// app.use(flash());
-
-//setting global variables for the flash messages
-// app.use(function(req, res, next){
-//     res.locals.success_msg = req.flash('success_msg');
-//     res.locals.error_msg = req.flash('error_msg');
-//     res.locals.error = req.flash('error');
-//     next();
-// });
-
-//set static folder
-app.use(express.static(path.join(__dirname, 'public')));
-
-//For Handlebars
-// app.set('views', './app/views')
-// app.engine('hbs', exphbs({ extname: '.hbs' defaultLayout: 'main' }));
-// app.set('view engine', '.hbs');
-
-// app.get('/', function(req, res) {
-//     res.send('Welcome to Passport with Sequelize');
-// });
- 
-//Models
-// var models = require('./app/models');
-// var models = require('./models');
-
-//Routes
-var authRoute = require('./app/routes/auth.js')(app,passport);
-
-//load passport strategies
-require('./app/config/passport/passport.js')(passport, models.user);
-
-//Sync database
-// models.sequelize.sync().then(function(){
-//     console.log('Nice database looks fine');
-// }).catch(function(err){
-//     console.log(err, 'something went wrong with the database update');
-// });
+// //For Handlebars
+app.set('views', './app/views')
+app.engine('hbs', engine({ extname: '.hbs' , defaultLayout: 'main'}));
+app.set('view engine', 'hbs');
 
 app.listen(5000, function(err) {
- 
     if (!err)
         console.log("Site is live");
     else console.log(err)
- 
-});
 
+});
