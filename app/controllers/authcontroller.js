84bb890bce1cdaef30c9e53b76813c84359e52bf
emma ingurat <e.ingurat@bsmart-technology.com>
2022-02-03 16:26:49 +0300

    added layout false fixes

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -1,20 +1,26 @@
 var exports = module.exports = {}
  
 exports.signup = function(req, res) {
-  res.render('../auth/register');
+	res.render('../app/views/register.hbs', { 
+		layout: false 
+	});
 }
 
 exports.signin = function(req, res) {
-    res.render('./auth/login');
+	res.render('../app/views/login.hbs', { 
+		layout: false 
+	});
 }
 
 exports.dashboard = function(req,res){
-	res.render('home'); 
+	res.render('../app/views/home.hbs'); 
 }
 
 exports.logout = function(req,res){
   req.session.destroy(function(err) {
-        res.redirect('./auth/login');
+	res.redirect('../app/views/login.hbs', {
+		layout: false 
+	});
   });
 
 }
