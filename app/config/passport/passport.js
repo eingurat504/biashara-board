93df380f0343c6fa75bd40fc288ee46f1ef68616
emma ingurat <e.ingurat@bsmart-technology.com>
2022-02-07 22:53:26 +0300

    passport usage fixes

--- app/config/passport/passport.js
+++ app/config/passport/passport.js
@@ -26,10 +26,7 @@ module.exports = function(passport, user) {
             usernameField: 'email',
             passwordField: 'password',
             passReqToCallback: true // allows us to pass back the entire request to the callback
-        },
-        function(req, email, password, done) {
-
-            console.log(req.body);
+        },function(req, email, password, done) {
 
             var email = req.body.email;
             var firstname = req.body.firstname;
@@ -80,12 +77,9 @@ module.exports = function(passport, user) {
         }
     ));
 
-
     //LOCAL SIGNIN
     passport.use('local-signin', new LocalStrategy(
-    
         {
-      
         // by default, local strategy uses username and password, we will override with email
         usernameField : 'email',
         passwordField : 'password',
@@ -101,25 +95,17 @@ module.exports = function(passport, user) {
               }
           
               User.findOne({ where : { email: email}}).then(function (user) {
-          
-                if (!user) {
-                  return done(null, false, { message: 'Email does not exist' });
-                }
-          
-                if (!isValidPassword(user.password,password)) {
-          
-                  return done(null, false, { message: 'Incorrect password.' });
-          
-                }
-          
-                var userinfo = user.get();
-          
-                return done(null,userinfo);
-          
+                    if (!user) {
+                      return done(null, false, { message: 'Email does not exist' });
+                    }
+                    if (!isValidPassword(user.password,password)) {
+                      return done(null, false, { message: 'Incorrect password.' });
+                    }
+                    var userinfo = user.get();
+                    return done(null,userinfo);
               }).catch(function(err){
-          
-                console.log("Error:",err);
-                return done(null, false, { message: 'Something went wrong with your Signin' });
+                    console.log("Error:",err);
+                    return done(null, false, { message: 'Something went wrong with your Signin' });
               });
       
         }
