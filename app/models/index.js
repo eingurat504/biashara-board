531c3ff9cae1a0e3797d45dee6f8ee192d81cb68
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-19 23:38:23 +0300

    model index fixes

--- app/models/index.js
+++ app/models/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var env = process.env.NODE_ENV || "development";
-var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
-var db = {};
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const basename = path.basename(__filename);
+const config = require(__dirname + '/../config/config.js');
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const db = {};
 
 fs
     .readdirSync(__dirname)
-    .filter(function(file) {
-        return (file.indexOf(".") !== 0) && (file !== "index.js");
+    .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
     })
     .forEach(function(file) {
         // var model = sequelize.import(path.join(__dirname, file));
