af61c8bb80e63a814f2da5a6595f2817176cd68c
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-22 08:04:57 +0300

    controller updates

--- app/controllers/boardcontroller.js
+++ app/controllers/boardcontroller.js
@@ -3,14 +3,14 @@ const Board = db.boards;
 
 async function create(req,res){
 
-    var title = req.body.title;
-    var description = req.body.description;
-    var status = req.body.status;
+    const title = req.body.title;
+    const description = req.body.description;
+    const status = req.body.status;
 
     //validation implementation
     req.checkBody('title', 'Title is required').notEmpty();
 
-    var errors = req.validationErrors();
+    const errors = req.validationErrors();
     
     if(errors){
         res.render('./card/create',{
@@ -18,7 +18,7 @@ async function create(req,res){
         });
     }else{
 
-        var data = {
+        const data = {
             title: req.body.title,
             description: req.body.description,
             status: req.body.status
@@ -27,15 +27,13 @@ async function create(req,res){
 
         Board.create(data).then(function(newEmail, created){
             if(!newEmail){
-                // return done(null, false);
-                console.log('failed to insert into the emails table');
+                return done(null, false);
             }
             if(newEmail){
                 return done(null, newEmail);
             }
         });
             
-    //     req.flash('success_msg','You are registered and can now login');   
         res.redirect('/dashboard');
     }
  
